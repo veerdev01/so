bff7e4b76a269e4fe376979696c7c9241825e235
istu56 <githubop94@gmail.com>
2026-03-17 22:36:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ======================================================
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/istu56/InflexMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/istu56/soko")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
