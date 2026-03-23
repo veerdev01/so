a33a92705f2658d13c4db5a5b36864ad3b431f14
istu56 <githubop94@gmail.com>
2026-03-23 11:52:29 +0530

    Update Youtube.py

--- IstkharMusic/platforms/Youtube.py
+++ IstkharMusic/platforms/Youtube.py
@@ -1,35 +1,3 @@
-
-# ======================================================================
-# ||                                                               ||
-# ||   ██████╗  █████╗ ██████╗ ██╗   ██╗███████╗███████╗██╗ ██████╗  ||
-# ||   ██╔══██╗██╔══██╗██╔══██╗██║   ██║██╔════╝██╔════╝██║██╔═══██╗ ||
-# ||   ██████╔╝███████║██████╔╝██║   ██║█████╗  ███████╗██║██║   ██║ ||
-# ||   ██╔══██╗██╔══██║██╔══██╗██║   ██║██╔══╝  ╚════██║██║██║▄▄ ██║ ||
-# ||   ██████╔╝██║  ██║██████╔╝╚██████╔╝███████╗███████║██║╚██████╔╝ ||
-# ||   ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚═╝ ╚══▀▀═╝  ||
-# ║    ▓▒░ ʙ ᴀ ʙ ɪ ᴇ sＩＱ ░▒▓  s ᴇ ᴄ ᴜ ʀ ᴇ  ▓▒░ ɴ ᴇ ᴛ ᴡ ᴏ ʀ ᴋ ░▒▓    ║
-# ||                                                               ||
-# ======================================================================
-# || PROJECT  : BABYMUSIC Public Music Repository                  ||
-# || AUTHOR   : BabiesIQ Team                                      ||
-# || REPO     : github.com/BABY-MUSIC/SPOTIFY_MUSIC                ||
-# || API      : www.babyapi.pro                                    ||
-# || TELEGRAM : t.me/BabiesIQ                                      ||
-# ----------------------------------------------------------------------
-# || LEGAL NOTICE                                                  ||
-# || Use / upload / modify at your own risk.                       ||
-# || Only config /.env edit allowed.                               ||
-# || Do not modify core files.                                     ||
-# || Keep this header if forked.                                   ||
-# || Dev not responsible for ban / damage / api block.             ||
-# ----------------------------------------------------------------------
-# || SECURITY                                                      ||
-# || Internal protection may exist.                                ||
-# || Unauthorized change may stop system.                          ||
-# || Use official API only -> www.babyapi.pro                      ||
-# ======================================================================
-
-
 import asyncio
 import os
 import re
