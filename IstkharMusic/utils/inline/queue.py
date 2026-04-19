59442f7048129741c8cf101c91604156ac8d68cb
veerdev01 <veerdev081@gmail.com>
2026-04-19 21:33:50 +0530

    Update queue.py

--- IstkharMusic/utils/inline/queue.py
+++ IstkharMusic/utils/inline/queue.py
@@ -68,11 +68,11 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="ᴊᴏɪɴ ɴᴏᴡ",
-                url=f"https://t.me/IamIstkhar"
+                url=f"https://t.me/+FOOeBfmPzm1iNGQ1"
             ),
             InlineKeyboardButton(
                 text="ɢʀᴏᴜᴘ ᴄʜᴧᴛ",
-                url="https://t.me/THUNDERDEVS"
+                url="https://t.me/+FOOeBfmPzm1iNGQ1"
             ),
         ],
         [
